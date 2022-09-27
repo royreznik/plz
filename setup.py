0b3b52b67a43715bc639773ce903c73f31591a59
Roy Reznik <royreznik@gmail.com>
2022-09-28 00:08:57 +0300

    Creating base folder structure

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from plz import __version__
+
+DEPENDENCIES = [
+    'pip-tools',
+    'virtualenv',
+    'click'
+]
+
+setup(
+    name='plz',
+    version=__version__,
+    author='Roy Reznik',
+    author_email='royreznik@gmail.com',
+    license='MIT License',
+    keywords='CLI wrapping over virtualenv and pip-tools',
+    packages=['plz'],
+    install_requires=DEPENDENCIES,
+)
